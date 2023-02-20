40225ce00c8c825b0cdfcea7a8b22bf426a8f1bd
Kamil Szerląg <kamilszerlag@gmail.com>
2023-02-20 16:56:43 +0100

    CM-27/add taking photo and uploading

--- src/screens/EmployeesListScreen/EmployeesListItem.tsx
+++ src/screens/EmployeesListScreen/EmployeesListItem.tsx
@@ -1,12 +1,13 @@
 import { Employee } from '@app/types';
 import { useNavigation } from '@react-navigation/native';
-import { View, StyleSheet, Pressable } from 'react-native';
+import { View, StyleSheet, Pressable, Image } from 'react-native';
 import { Typography } from '@app/components/Typography';
 import { GlobalStyles } from '@app/constants/styles';
+import avatarImg from '@app/assets/images/avatar.png';
 
 type EmployeesListItemProps = Pick<
   Employee,
-  'id' | 'firstName' | 'lastName' | 'jobPosition' | 'salary'
+  'id' | 'firstName' | 'lastName' | 'jobPosition' | 'salary' | 'imageUri'
 >;
 
 export const EmployeesListItem = ({
@@ -15,13 +16,20 @@ export const EmployeesListItem = ({
   lastName,
   jobPosition,
   salary,
+  imageUri,
 }: EmployeesListItemProps) => {
   const navigation = useNavigation();
   return (
     <Pressable
       style={styles.item}
       onPress={() => navigation.navigate('EmployeeDetails', { employeeId: id })}>
-      <View style={styles.image} />
+      {imageUri ? (
+        <View style={styles.imgContainer}>
+          <Image source={{ uri: imageUri }} style={styles.image} />
+        </View>
+      ) : (
+        <Image source={avatarImg} style={styles.image} />
+      )}
       <View>
         <Typography type="normal" style={[styles.nameText, styles.paddingText]}>
           {`${firstName} ${lastName}`}
@@ -60,11 +68,12 @@ const styles = StyleSheet.create({
   salaryText: {
     color: GlobalStyles.colors.blue100,
   },
-  image: {
-    width: 50,
-    height: 50,
-    backgroundColor: '#ddd',
+  imgContainer: {
+    overflow: 'hidden',
     borderRadius: 50,
-    paddingRight: 20,
+  },
+  image: {
+    width: 80,
+    height: 84,
   },
 });
